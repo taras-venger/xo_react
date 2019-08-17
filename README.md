d6db1aade5b60b88462843ebb68dcf9a1f293970
Taras Venger <taras-venger@users.noreply.github.com>
2019-08-17 19:58:23 +0300

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-Tic Tac Toe.
-My first React app:
-https://taras-venger.github.io/xo_react/
---
+++ precache-manifest.857d0b742b0bd0b32697a2bfcae951ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33484f7005b8e4500f8d3eb49ad77f45",
+    "url": "/xo_react/index.html"
+  },
+  {
+    "revision": "47d735ce48b35b090f26",
+    "url": "/xo_react/static/css/main.727f587d.chunk.css"
+  },
+  {
+    "revision": "ac20d6fe63240a0f2e36",
+    "url": "/xo_react/static/js/2.72cd42a7.chunk.js"
+  },
+  {
+    "revision": "47d735ce48b35b090f26",
+    "url": "/xo_react/static/js/main.1c69d09e.chunk.js"
+  },
+  {
+    "revision": "71132a92f6510652524d",
+    "url": "/xo_react/static/js/runtime~main.78d582f6.js"
+  },
+  {
+    "revision": "050621b91280727dbfee14a1fe2c346c",
+    "url": "/xo_react/static/media/img.050621b9.jpg"
+  }
+]);
